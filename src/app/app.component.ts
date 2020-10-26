ab9f1a48d5774a61ef5c795b7d11e7a8ce6b1a4f
akshay khairnar <khairnaraksh@gmail.com>
2020-10-26 22:19:55 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'qrcodefileSend';
+  public myAngularxQrCode: string = null;
+  public QRDATa: string = 'Hello QR';
+  public href: string;
+
+  /*************  Button 1 Code  ************/
+  getBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    var dataURL = canvas.toDataURL("image/png");
+    return dataURL;
+  }
+
+  download() {
+    const qrcode = document.getElementById('qrcode') as HTMLElement;
+    let imageData = this.getBase64Image(qrcode.firstChild.firstChild);
+    const link = document.createElement('a');
+    link.href = imageData;
+    link.download = 'Qrcode';
+    link.click();
+  }
+  /*************  Button 2 Code  END ************/
+
+  /*************  Button 2 Code  ************/
+  saveAsImage(parent) {
+    const parentElement = parent.qrcElement.nativeElement.querySelector("img").src;
+    // converts base 64 encoded image to blobData
+    let blobData = this.convertBase64ToBlob(parentElement);
+
+    // saves as image
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) { //IE
+      window.navigator.msSaveOrOpenBlob(blobData, 'Qrcode');
+    } else { // chrome
+      const blob = new Blob([blobData], { type: "image/png" });
+      const url = window.URL.createObjectURL(blob);
+      // window.open(url);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = 'Qrcode';
+      link.click();
+    }
+  }
+
+  private convertBase64ToBlob(Base64Image: any) {
+    // SPLIT INTO TWO PARTS
+    const parts = Base64Image.split(';base64,');
+    // HOLD THE CONTENT TYPE
+    const imageType = parts[0].split(':')[1];
+    // DECODE BASE64 STRING
+    const decodedData = window.atob(parts[1]);
+    // CREATE UNIT8ARRAY OF SIZE SAME AS ROW DATA LENGTH
+    const uInt8Array = new Uint8Array(decodedData.length);
+    // INSERT ALL CHARACTER CODE INTO UINT8ARRAY
+    for (let i = 0; i < decodedData.length; ++i) {
+      uInt8Array[i] = decodedData.charCodeAt(i);
+    }
+    // RETURN BLOB IMAGE AFTER CONVERSION
+    return new Blob([uInt8Array], { type: imageType });
+  }
+  /*************  Button 2 Code End ************/
+
+
 }
